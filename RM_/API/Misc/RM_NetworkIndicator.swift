69e94a65cc1c2beaf80d96580f549fac80522a4f
OlehKulykov <info@resident.name>
2016-04-27 01:47:52 +0200

    Added NetworkIndicator tests

--- RM_/API/Misc/RM_NetworkIndicator.swift
+++ RM_/API/Misc/RM_NetworkIndicator.swift
@@ -9,25 +9,66 @@
 import Foundation
 import UIKit
 
+/**
+Threadsafe application network indicator.
+Used `UIApplication.sharedApplication().networkActivityIndicatorVisible` and could be
+updated recursivly from any thread.
+Use `visible` static variable to change visibility.
+
+- Note: Based on counter logic, so if you want to change this value you need to set reverse value same times.
+
+Example: 
+```swift
+	RM_NetworkIndicator.visible = true // visible
+	RM_NetworkIndicator.visible = true // visible
+	... 
+	RM_NetworkIndicator.visible = false // still visible
+	RM_NetworkIndicator.visible = false // now it's invisible
+```
+*/
 class RM_NetworkIndicator {
 
-	private static var visibility: Int = 0 {
+	/// Counts number of visible calls. Positive value is visible, otherwice invisible.
+	/// - Warning: Get/set value from main thread. Min value is zero - invisible.
+	private static var visibilityCounter: Int = 0 {
 		didSet {
-			UIApplication.sharedApplication().networkActivityIndicatorVisible = visibility > 0
+			UIApplication.sharedApplication().networkActivityIndicatorVisible = visibilityCounter > 0
 		}
 	}
 
+
+	/// Increment or decrement visibility counter
+	/// - Parameter visible: Required visibility. `true` - increment, `false` - decrement to min, zero value.
+	private static func updateVisibilityCounter(visible: Bool) {
+		let newValue = visible ? visibilityCounter + 1 : visibilityCounter - 1
+		visibilityCounter = max(newValue, 0)
+	}
+
+
+	/**
+	Set/get network indicator visibility.
+	Threadsafe. Actual value will be set/get within main thread.
+
+	- Note: Based on counter logic, so if you want to change this value you need to set reverse value same times.
+	*/
 	static var visible: Bool {
 		get {
-			return visibility > 0
+			if NSThread.isMainThread() {
+				return visibilityCounter > 0
+			} else {
+				var value = false
+				dispatch_sync(dispatch_get_main_queue(), {
+					value = visibilityCounter > 0
+				})
+				return value
+			}
 		}
 		set {
-			let increment = newValue ? 1 : -1
 			if NSThread.isMainThread() {
-				visibility = max(visibility + increment, 0)
+				updateVisibilityCounter(newValue)
 			} else {
 				dispatch_async(dispatch_get_main_queue(), {
-					visibility = max(visibility + increment, 0)
+					updateVisibilityCounter(newValue)
 				})
 			}
 		}
---
+++ RM_Tests/API/Misc/RM_NetworkIndicatorTests.swift
@@ -0,0 +1,78 @@
+//
+//  RM_NetworkIndicatorTests.swift
+//  RM_
+//
+//  Created by Oleh Kulykov on 27/04/16.
+//  Copyright © 2016 Oleh Kulykov. All rights reserved.
+//
+
+import XCTest
+@testable import RM_
+
+class RM_NetworkIndicatorTests: XCTestCase {
+
+	func toInvisible() {
+		for _ in 0...5 {
+			RM_NetworkIndicator.visible = false
+		}
+		XCTAssertFalse(RM_NetworkIndicator.visible, "Should be invisible")
+	}
+
+	func testSingle() {
+		toInvisible()
+
+		RM_NetworkIndicator.visible = true
+		XCTAssertTrue(RM_NetworkIndicator.visible, "Should be visible")
+
+		RM_NetworkIndicator.visible = false
+		XCTAssertFalse(RM_NetworkIndicator.visible, "Should be invisible")
+	}
+
+	func testMultiple() {
+		toInvisible()
+
+		RM_NetworkIndicator.visible = true
+		RM_NetworkIndicator.visible = true
+		XCTAssertTrue(RM_NetworkIndicator.visible, "Should be visible")
+
+		RM_NetworkIndicator.visible = false
+		XCTAssertTrue(RM_NetworkIndicator.visible, "Should be visible")
+		RM_NetworkIndicator.visible = false
+		XCTAssertFalse(RM_NetworkIndicator.visible, "Should be invisible")
+	}
+
+	func testMultipleMultiThreaded() {
+		toInvisible()
+
+		RM_NetworkIndicator.visible = true
+		XCTAssertTrue(RM_NetworkIndicator.visible, "Should be visible")
+
+		let expectation = self.expectationWithDescription("Dummy timeout.")
+
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), {
+			NSThread.sleepForTimeInterval(0.1)
+			RM_NetworkIndicator.visible = true
+			XCTAssertTrue(RM_NetworkIndicator.visible, "Should be visible")
+		})
+
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), {
+			NSThread.sleepForTimeInterval(0.2)
+			RM_NetworkIndicator.visible = false
+			XCTAssertTrue(RM_NetworkIndicator.visible, "Should be visible")
+		})
+
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), {
+			NSThread.sleepForTimeInterval(0.3)
+			RM_NetworkIndicator.visible = false
+			XCTAssertFalse(RM_NetworkIndicator.visible, "Should be invisible")
+			expectation.fulfill()
+		})
+
+		self.waitForExpectationsWithTimeout(10) { error in
+			XCTAssertFalse(RM_NetworkIndicator.visible, "Should be invisible")
+			if let error = error {
+				XCTFail("Create user in background: \(error)")
+			}
+		}
+	}
+}
