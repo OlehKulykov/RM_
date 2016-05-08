be6c3dd4b17fd840e2555625078652ba3c699dbc
OlehKulykov <info@resident.name>
2016-05-08 12:01:17 +0200

    UIImageView extension with load from URL functionality

---
+++ RM_/View/Extensions/UIImageView+LoadFromURL.swift
@@ -0,0 +1,67 @@
+//
+//  UIImageView+LoadFromURL.swift
+//  RM_
+//
+//  Created by Oleh Kulykov on 27/04/16.
+//  Copyright © 2016 Oleh Kulykov. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+
+	private struct AssociatedKey {
+		static var imageUrlKey = "imageUrl"
+	}
+
+	/**
+	Public getter of the loaded image.
+	*/
+	private(set) var imageUrl: NSURL? {
+		get {
+			return objc_getAssociatedObject(self, &AssociatedKey.imageUrlKey) as? NSURL
+		}
+		set {
+			objc_setAssociatedObject(self, &AssociatedKey.imageUrlKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+		}
+	}
+
+
+	private func loadImageFromURL(url: NSURL) {
+		let task = NSURLSession.sharedSession().dataTaskWithURL(url) { [weak self] (data, response, error) in
+			guard let
+				receivedData = data,
+				receivedImage = UIImage(data: receivedData)
+				else {
+					return
+			}
+			dispatch_async(dispatch_get_main_queue(), {
+				if let imageUrl = self?.imageUrl where imageUrl == url {
+					self?.image = receivedImage
+				}
+				self?.setNeedsDisplay()
+			})
+		}
+		task.resume()
+	}
+
+
+	/**
+	Load image from url with placeholder image while loading in progress
+	- Parameter url: Image URL for download
+	- Parameter placeholder: Placeholder image for display during loading
+	*/
+	public func loadImageFromURL(url: NSURL?, placeholder: UIImage?) {
+		self.image = placeholder
+		self.imageUrl = url
+
+		guard let url = url else {
+			return
+		}
+
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), { [weak self] in
+			self?.loadImageFromURL(url)
+			})
+	}
+	
+}
