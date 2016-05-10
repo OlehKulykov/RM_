59758fa0a981825eb8a188715e6967169a1694c4
OlehKulykov <info@resident.name>
2016-05-10 08:49:35 +0200

    Add view edged borders extension

---
+++ RM_/View/Extensions/UIView+EdgedBorders.swift
@@ -0,0 +1,83 @@
+//
+//  UIView+EdgedBorders.swift
+//  RM_
+//
+//  Created by Oleh Kulykov on 27/04/16.
+//  Copyright © 2016 Oleh Kulykov. All rights reserved.
+//
+
+
+import UIKit
+
+extension UIView {
+
+	func addBorders(edges: UIRectEdge, colour: UIColor, thickness: CGFloat) -> [UIView] {
+
+		func border() -> UIView {
+			let border = UIView(frame: CGRectZero)
+			border.backgroundColor = colour
+			border.translatesAutoresizingMaskIntoConstraints = false
+			return border
+		}
+
+		var borders = [UIView]()
+		if edges.contains(.Top) || edges.contains(.All) {
+			let top = border()
+			self.addSubview(top)
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-(0)-[top(==thickness)]",
+				options: [],
+				metrics: ["thickness": thickness],
+				views: ["top": top]))
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-(0)-[top]-(0)-|",
+				options: [],
+				metrics: nil,
+				views: ["top": top]))
+			borders.append(top)
+		}
+
+		if edges.contains(.Left) || edges.contains(.All) {
+			let left = border()
+			self.addSubview(left)
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-(0)-[left(==thickness)]",
+				options: [],
+				metrics: ["thickness": thickness],
+				views: ["left": left]))
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-(0)-[left]-(0)-|",
+				options: [],
+				metrics: nil,
+				views: ["left": left]))
+			borders.append(left)
+		}
+
+		if edges.contains(.Right) || edges.contains(.All) {
+			let right = border()
+			self.addSubview(right)
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:[right(==thickness)]-(0)-|",
+				options: [],
+				metrics: ["thickness": thickness],
+				views: ["right": right]))
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-(0)-[right]-(0)-|",
+				options: [],
+				metrics: nil,
+				views: ["right": right]))
+			borders.append(right)
+		}
+
+		if edges.contains(.Bottom) || edges.contains(.All) {
+			let bottom = border()
+			self.addSubview(bottom)
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bottom(==thickness)]-(0)-|",
+				options: [],
+				metrics: ["thickness": thickness],
+				views: ["bottom": bottom]))
+			self.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-(0)-[bottom]-(0)-|",
+				options: [],
+				metrics: nil,
+				views: ["bottom": bottom]))
+			borders.append(bottom)
+		}
+		return borders
+	}
+	
+}
+
