e936dadaf5e5709937fe5db7f6c84f30939d8888
OlehKulykov <info@resident.name>
2016-05-10 08:45:35 +0200

    Describe repeated string initialization

--- RM_/Model/Extensions/String+RepeatedString.swift
+++ RM_/Model/Extensions/String+RepeatedString.swift
@@ -9,7 +9,16 @@
 import Foundation
 
 extension String {
+
+	/**
+	Initialize string with number of repeated string.
 	
+	- Parameter count: Number of repeated parts count.
+	
+	- Parameter repeated: Repeated string part.
+	
+	- Returns: String with the number of repeated parts count or empty string.
+	*/
 	public init(count: Int, repeatedValue repeated: String) {
 		var s = ""
 		for _ in 0..<count {
