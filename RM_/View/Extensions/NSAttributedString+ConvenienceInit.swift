4004fa9d276dda1d0ebc03b9edcaba410a97e404
OlehKulykov <info@resident.name>
2016-05-12 12:47:18 +0200

    Additional NSAttributedString initializers

--- RM_/View/Extensions/NSAttributedString+ConvenienceInit.swift
+++ RM_/View/Extensions/NSAttributedString+ConvenienceInit.swift
@@ -10,6 +10,24 @@ import UIKit
 
 extension NSAttributedString {
 
+	/**
+	- Parameter lineSpacing: The distance in points between the bottom of one line fragment and the top of the next.
+	*/
+	public convenience init(string: String, font: UIFont, color: UIColor, lineSpacing: CGFloat, textAlignment: NSTextAlignment, kerning: CGFloat = 0) {
+		var attributes = [NSFontAttributeName: font, NSForegroundColorAttributeName: color]
+		if kerning != 0 {
+			attributes[NSKernAttributeName] = NSNumber(float: Float(kerning))
+		}
+
+		if lineSpacing != 0 {
+			let style = NSMutableParagraphStyle()
+			style.lineSpacing = lineSpacing
+			style.alignment = textAlignment
+			attributes[NSParagraphStyleAttributeName] = style
+		}
+		self.init(string: string, attributes: attributes)
+	}
+
 	public convenience init(string: String, font: UIFont, color: UIColor, kerning: CGFloat = 0) {
 		var attributes = [NSFontAttributeName: font, NSForegroundColorAttributeName: color]
 		if kerning != 0 {
