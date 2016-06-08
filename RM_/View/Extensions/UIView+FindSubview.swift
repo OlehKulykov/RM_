2eb6e55f4d2a3b08786a9605d8b42e0683ef27f4
OlehKulykov <info@resident.name>
2016-06-08 20:16:22 +0200

    Find subview extenstion

---
+++ RM_/View/Extensions/UIView+FindSubview.swift
@@ -0,0 +1,31 @@
+//
+//  UIView+FindSubview.swift
+//  RM_
+//
+//  Created by Oleh Kulykov on 27/04/16.
+//  Copyright © 2016 Oleh Kulykov. All rights reserved.
+//
+
+
+import UIKit
+
+extension UIView {
+
+	/**
+	Recursively find subview of type `T` and optional tag value.
+
+	- Parameter tag: Optional tag of subview.
+
+	- Returns: Located subview or nil if not found.
+	*/
+	func findSubView<T where T: UIView>(tag: Int = 0) -> T? {
+		for subview in subviews {
+			if let result = subview as? T where result.tag == tag {
+				return result
+			} else if let result: T = subview.findSubView(tag) {
+				return result
+			}
+		}
+		return nil
+	}
+}
