6345274439d0a0deb194fa5a1e2395fbfc3bd5eb
OlehKulykov <info@resident.name>
2016-06-08 20:24:30 +0200

    NSDecimalNumber price string generation

---
+++ RM_/Model/Extensions/NSDecimalNumber+PriceString.swift
@@ -0,0 +1,51 @@
+//
+//  NSDecimalNumber+PriceString.swift
+//  RM_
+//
+//  Created by Oleh Kulykov on 08/06/16.
+//  Copyright © 2016 Oleh Kulykov. All rights reserved.
+//
+
+
+import Foundation
+
+extension NSDecimalNumber {
+
+	/**
+	Default number formatter.
+	*/
+	private static func currencyFormatter(currencySymbol: String?, currencySuffix: String?) -> NSNumberFormatter {
+		let currencyFormatter = NSNumberFormatter()
+		currencyFormatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+		currencyFormatter.locale = NSLocale.currentLocale()
+
+		var symbol = currencySymbol
+		if symbol == nil {
+			symbol = "$"
+		}
+
+		if currencySuffix != nil {
+			currencyFormatter.currencySymbol = symbol! + currencySuffix!
+		} else {
+			currencyFormatter.currencySymbol = symbol!
+		}
+
+		// no grouping, eg " 12'234.99 "
+		currencyFormatter.usesGroupingSeparator = false
+		currencyFormatter.currencyGroupingSeparator = ""
+
+		// no ".00" at the end
+		//		currencyFormatter.alwaysShowsDecimalSeparator = false
+		//		currencyFormatter.minimumFractionDigits = 0
+		return currencyFormatter;
+	}
+
+	/**
+	Generate price string with currency symbol and separated currency suffix
+	Example: <currencySymbol><currencySuffix><44.99>
+	*/
+	internal func priceString(currencySymbol: String? = nil, currencySuffix: String? = nil) -> String {
+		return NSDecimalNumber.currencyFormatter(currencySymbol, currencySuffix: currencySuffix).stringFromNumber(self)!
+	}
+	
+}
